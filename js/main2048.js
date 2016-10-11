7d6c0e6776198c0e446fc40997ba5155f2d33182
ink0101 <cftt2016@gmail.com>
2016-10-11 12:15:54 +0800

    UI finished

--- js/main2048.js
+++ js/main2048.js
@@ -5,5 +5,22 @@ var board = new Array();
 var score = 0;
 
 $(document).ready(function () {
-	
+	newgame();
 });
+
+function newgame() {
+	// 初始化棋盘格
+	init();
+	// 随机两个格子生成数字
+}
+
+function init() {
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+			var gridCell = $('#grid-cell-'+i+'-'+j);
+			// 对gridCell的位置进行计算,使用两个新的函数
+			gridCell.css('top',getPosTop(i,j));
+			gridCell.css('left',getPosLeft(i,j));
+		}
+	}
+}
